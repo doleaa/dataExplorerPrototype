4e97ba365ca5a411d58a036ae3c46cab5142b9c1
doleaa <dolea15@gmail.com>
2021-07-10 18:17:29 +0100

    Setup Grid layout with react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Col, Container, Row} from 'react-bootstrap';
 
 function App() {
+  const threeOnARowSize: number = 4;
+  const twoOnARowSize: number = 6;
+  const oneOnARowSize: number = 12;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>dfsafdsdjsfhldskfhdlskh</div>
       </header>
+      <body>
+        <Container>
+          <Row>
+            <Col xs={oneOnARowSize} sm={oneOnARowSize} md={oneOnARowSize}>
+              <img src={logo} className="App-logo" alt="logo" />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={twoOnARowSize} sm={twoOnARowSize} md={twoOnARowSize}>
+              <img src={logo} className="App-logo" alt="logo" />
+            </Col>
+            <Col xs={twoOnARowSize} sm={twoOnARowSize} md={twoOnARowSize}>
+              <img src={logo} className="App-logo" alt="logo" />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={threeOnARowSize} sm={threeOnARowSize} md={threeOnARowSize}>
+              <img src={logo} className="App-logo" alt="logo" />
+            </Col>
+            <Col xs={threeOnARowSize} sm={threeOnARowSize} md={threeOnARowSize}>
+              <img src={logo} className="App-logo" alt="logo" />
+            </Col>
+            <Col xs={threeOnARowSize} sm={threeOnARowSize} md={threeOnARowSize}>
+              <img src={logo} className="App-logo" alt="logo" />
+            </Col>
+          </Row>
+        </Container>
+        <p>
+          Edit <code>src/App.tsx</code> for further spinception
+        </p>
+      </body>
     </div>
   );
 }
